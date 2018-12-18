83ed31c5391dbfd5ce78edf51d9ac85329dfa2ff
yaochuhong <yaochuhong@panath.cn>
2018-12-18 10:58:20 +0800

    add python study example
    Signed-off-by: yaochuhong <yaochuhong@panath.cn>

---
+++ example/for.py
@@ -0,0 +1,14 @@
+for val in range(5):
+    print('range1 val is '+str(val))
+
+print('exit for1')
+
+for val in range(10, 12):
+    print('range2 val is '+str(val))
+
+print('exit for2')
+
+for val in range(10, 12, 2):
+    print('range3 val is '+str(val))
+
+print('exit for3')
---
+++ example/while.py
@@ -0,0 +1,9 @@
+while True:
+    print('Please input your name.')
+    name = input()
+    if name == 'ych':
+        break
+
+print('exit')
+
+
